6cf8f9970747589170587f7dbc2e7e1ac763d5c7
Ashwin Sheoran <88393756+AshwinSheoran02@users.noreply.github.com>
2022-07-28 19:24:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const  button = document.querySelector('button')
 
-const TEST_COUNT = 5
+const TEST_COUNT = 1
 const progress = document.querySelector('.progress')
 const speedText = document.querySelector('.speed-text')
 let test_results = []
 
 var imageAddr = "https://raw.githubusercontent.com/AshwinSheoran02/Internet-Speed-Check-Website/main/test.jpg"
-var size = 4362493 ; //image size in bytes
+var size = 4362493 * 8 ; //image size in bytes
 
 //let startTime = Date.now()
 function loadImage(){
@@ -18,7 +18,7 @@ function loadImage(){
             download.onload = function(){
                 let endTime = Date.now()
                 console.log(endTime - startTime);
-                resolve(endTime - startTime)
+                resolve( endTime - startTime)
             }
             download.onerror = function(err){
                 reject(err)
@@ -32,11 +32,11 @@ function loadImage(){
 async function getLoadSpeed(){
     let loadTime = await loadImage()
     //if(loadTime < 1) loadTime = 1
-    let speed_bps = size / loadTime
+    let speed_bps = size / (loadTime * 1000)
     let speed_kbps = speed_bps / 1024
     let speed_Mbps = speed_kbps / 1024
 
-    return speed_kbps
+    return speed_Mbps
 }
 
 function getAvgSpeed(){
@@ -50,6 +50,6 @@ button.addEventListener('click', async function(){
         let speed = await getLoadSpeed()
         test_results.push(speed)
         progress.style.width = ((i + 1) / TEST_COUNT * 100) + '%'
-        speedText.innerText = getAvgSpeed().toFixed(2) + ' kbps'
+        speedText.innerText = getAvgSpeed().toFixed(2) + ' Mbps'
     }
 })
